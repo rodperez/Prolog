d81a4660d92483caeea74ac108333fee2668793a
Rodrigo <rodrigogastonperez15@gmail.com>
2017-06-13 14:08:37 -0300

    Entrega 1.5
    Correción de errores en los puntos 1, 2, 3 y 4.

--- Tp-Prolog-Propiedades.pl
+++ Tp-Prolog-Propiedades.pl
@@ -6,46 +6,46 @@ persona(maría).
 persona(pedro).
 persona(chamaleon).
 
-propiedad(tinsmithCircle_1774).
-propiedad(av_moreno_708).
-propiedad(av_siempreViva_742).
-propiedad(calle_falsa_123).
+propiedad(Propiedad):- tiene(Propiedad,_).
 
-tiene(av_moreno_708, precioAlquiler(2000)).
 tiene(av_moreno_708, ambiente(7)).
 tiene(av_moreno_708, jardin).
-tiene(av_moreno_708, metrosCúbicosDeLaPileta(30)).
-tiene(tinsmithCircle_1774, precioAlquiler(700)).
+tiene(av_moreno_708, pileta(30)).
 tiene(tinsmithCircle_1774, ambiente(3)).
 tiene(tinsmithCircle_1774, jardin).
-tiene(av_siempreViva_742, precioAlquiler(1000)).
 tiene(av_siempreViva_742, ambiente(4)).
 tiene(av_siempreViva_742, jardin).
-tiene(calle_falsa_123, precioAlquiler(200)).
 tiene(calle_falsa_123, ambiente(3)).
 
-/* Punto 1 Funciona Perfecto */
+precioPropiedad(av_moreno_708,2000).
+precioPropiedad(tinsmithCircle_1774,700).
+precioPropiedad(av_siempreViva_742,1000).
+precioPropiedad(calle_falsa_123,200).
 
-existePropiedadConPiletaDe30Metros(Propiedades) :-
-  findall(Propiedad, tienePiletaDe30Metros(Propiedad), Propiedades).
+/*Punto 1
+  consulta:
+  tiene(Propiedad, pileta(30)).
+  respuesta: Propiedad = av_moreno_708.*/
 
-tienePiletaDe30Metros(Propiedad) :-
-  tiene(Propiedad, metrosCúbicosDeLaPileta(30)).
+/*Punto 2
+  consulta:
+  tiene(Propiedad, ambiente(Amb)), tiene(Propiedad2,ambiente(Amb)), Propiedad \= Propiedad2.
+  respuesta : Propiedad = tinsmithCircle_1774,
+              ambiente = 3,
+              Propiedad2 = calle_falsa_123.*/
 
-/* Punto 2 Funciona Perfecto */
+/*Punto 3
+  consulta : quiere(pedro,Deseo).
+  respuesta: Deseo = ambiente(2),
+             Deseo = pileta(15).*/
 
-mismaCantidadDeAmbientes(Propiedad, Propiedad2) :-
-  propiedad(Propiedad), propiedad(Propiedad2),
-  tiene(Propiedad, ambiente(Amb)),
-  tiene(Propiedad2,ambiente(Amb)),
-  Propiedad \= Propiedad2.
-
-/* Punto 3 Funciona Perfeco */
-
-quiere(carlos, caracteristicas(ambiente(3), jardin)).
-quiere(pedro, caracteristicas(ambiente(2), metrosCúbicosDeLaPileta(15))).
-quiere(maría, caracteristicas(ambiente(2), metrosCúbicosDeLaPileta(15))).
-quiere(ana, caracteristicas(metrosCúbicosDeLaPileta(100))).
+quiere(carlos, ambiente(3)).
+quiere(carlos, jardin).
+quiere(pedro, ambiente(2)).
+quiere(pedro, pileta(15)).
+quiere(maría, ambiente(2)).
+quiere(maría, pileta(15)).
+quiere(ana, pileta(100)).
 
 quiere(chamaleon,Caracteristicas):-
   persona(Persona),
@@ -54,22 +54,93 @@ quiere(chamaleon,Caracteristicas):-
 
 /* punto 4 */
 
-cumplePropiedad(Propiedad,Deseo):-
-  propiedadDeseada(Propiedad,Deseo).
+cumple(Propiedad,Caracteristica):-
+  quiere(_,Caracteristica),
+    tiene(Propiedad,Caracteristica).
 
-propiedadDeseada(Propiedad,tiene(Propiedad,Exterior)):-
-    tiene(Propiedad,Exterior).
+cumple(Propiedad,ambiente(AmbientesDeseados)):-
+  quiere(_,ambiente(AmbientesDeseados)),
+  tiene(Propiedad,ambiente(AmbientesOriginales)),
+  AmbientesOriginales >= AmbientesDeseados.
 
-propiedadDeseada(Propiedad,tiene(Propiedad,precioAlquiler(Precio))):-
-    tiene(Propiedad,precioAlquiler(Precio)).
+cumple(Propiedad,pileta(MetrosCúbicosDeseados)):-
+  quiere(_,pileta(MetrosCúbicosDeseados)),
+  tiene(Propiedad, pileta(MetrosCúbicosOriginales)),
+  MetrosCúbicosOriginales >= MetrosCúbicosDeseados.
 
-propiedadDeseada(Propiedad,tiene(Propiedad,ambiente(Ambiente))):-
-  tiene(Propiedad,ambiente(Amb)),
-  cantOriginalMayorOIgualALaDeseada(Amb,Ambiente).
+/*
+-? cumple(Propiedad,ambiente(2)).
+Propiedad = av_moreno_708 ;
+Propiedad = tinsmithCircle_1774 ;
+Propiedad = av_siempreViva_742 ;
+Propiedad = calle_falsa_123 ;
+Propiedad = av_moreno_708 ;
+Propiedad = tinsmithCircle_1774 ;
+Propiedad = av_siempreViva_742 ;
+Propiedad = calle_falsa_123 ;
+Propiedad = av_moreno_708 ;
+Propiedad = tinsmithCircle_1774 ;
+Propiedad = av_siempreViva_742 ;
+Propiedad = calle_falsa_123 ;
+Propiedad = av_moreno_708 ;
+Propiedad = tinsmithCircle_1774 ;
+Propiedad = av_siempreViva_742 ;
+Propiedad = calle_falsa_123 ;
 
-propiedadDeseada(Propiedad,tiene(Propiedad, metrosCúbicosDeLaPileta(MetrosCúbicos))):-
-  tiene(Propiedad, metrosCúbicosDeLaPileta(MC)),
-  cantOriginalMayorOIgualALaDeseada(MC,MetrosCúbicos).
+  Punto 5
+-?quiere(pedro,Deseo),cumple(Propiedad,Deseo).
+Deseo = ambiente(2),
+Propiedad = av_moreno_708 ;
+Deseo = ambiente(2),
+Propiedad = tinsmithCircle_1774 ;
+Deseo = ambiente(2),
+Propiedad = av_siempreViva_742 ;
+Deseo = ambiente(2),
+Propiedad = calle_falsa_123 ;
+Deseo = ambiente(2),
+Propiedad = av_moreno_708 ;
+Deseo = ambiente(2),
+Propiedad = tinsmithCircle_1774 ;
+Deseo = ambiente(2),
+Propiedad = av_siempreViva_742 ;
+Deseo = ambiente(2),
+Propiedad = calle_falsa_123 ;
+Deseo = ambiente(2),
+Propiedad = av_moreno_708 ;
+Deseo = ambiente(2),
+Propiedad = tinsmithCircle_1774 ;
+Deseo = ambiente(2),
+Propiedad = av_siempreViva_742 ;
+Deseo = ambiente(2),
+Propiedad = calle_falsa_123 ;
+Deseo = ambiente(2),
+Propiedad = av_moreno_708 ;
+Deseo = ambiente(2),
+Propiedad = tinsmithCircle_1774 ;
+Deseo = ambiente(2),
+Propiedad = av_siempreViva_742 ;
+Deseo = ambiente(2),
+Propiedad = calle_falsa_123 ;
+Deseo = pileta(15),
+Propiedad = av_moreno_708 ;
+Deseo = pileta(15),
+Propiedad = av_moreno_708 ;
+Deseo = pileta(15),
+Propiedad = av_moreno_708 ;
+Deseo = pileta(15),
+Propiedad = av_moreno_708 ;
 
-cantOriginalMayorOIgualALaDeseada(Original,Deseada):-
-  Original >= Deseada.
+    Punto 6
+cumple(av_moreno_708,Deseo).
+Deseo = jardin ;
+Deseo = jardin ;
+Deseo = ambiente(3) ;
+Deseo = ambiente(2) ;
+Deseo = ambiente(2) ;
+Deseo = ambiente(3) ;
+Deseo = ambiente(2) ;
+Deseo = ambiente(2) ;
+Deseo = pileta(15) ;
+Deseo = pileta(15) ;
+Deseo = pileta(15) ;
+Deseo = pileta(15) ;*/
